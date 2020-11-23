bb57c7aaf5b7d4b0d913f868dbf43ab68ed9eab7
deeprob <deepro.2.4@gmail.com>
2020-11-23 14:59:07 -0500

    AddedResults

--- GBClassifier/model.py
+++ GBClassifier/model.py
@@ -7,14 +7,14 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.metrics import precision_score,recall_score,f1_score,confusion_matrix,accuracy_score
-from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.pipeline import Pipeline
 from tqdm import tqdm
 
 
-class SVM:
+class GBC:
     
-    def __init__(self,Xtrain,Xtest,ytrain,ytest,random_seed=None,pca_comp=20,regC=1,kern='rbf',optimize=False,verbose=True):
+    def __init__(self,Xtrain,Xtest,ytrain,ytest,random_seed=None,pca_comp=20,nest=15,lrate=0.1,mdepth=3,ssample=1,optimize=False,verbose=True):
         np.random.seed(random_seed)
         
         self.Xtrain = Xtrain
@@ -22,7 +22,7 @@ class SVM:
         self.ytrain = ytrain
         self.ytest = ytest
         
-        pipeline = self._make_pipeline(pca_comp,regC,kern,random_seed)
+        pipeline = self._make_pipeline(pca_comp,nest,lrate,mdepth,ssample)
         
         self.model = pipeline.fit(self.Xtrain,self.ytrain)
         self.ypredtrain = self.model.predict(self.Xtrain)
@@ -42,9 +42,9 @@ class SVM:
             print('-'*5+'Hyperparameter Optimization'+'-'*5)
 
             parameters = {'pca__n_components':[1,5,20,40],
-                         'SVM__C':[1,20,100],
-                         'SVM__gamma':['scale','auto',0.1],
-                         'SVM__kernel':['linear','rbf']}
+                         'GBC__n_estimators':[15,25,100],
+                         'GBC__learning_rate':[0.1,0.5,1],
+                         'GBC__max_depth':[1,3,5]}
 
             self.grid = GridSearchCV(pipeline, param_grid=parameters, cv=3, n_jobs=-1,scoring='accuracy',verbose=10)
             self.grid.fit(self.Xtrain, self.ytrain)
@@ -61,7 +61,7 @@ class SVM:
         
                 
         
-    def _make_pipeline(self,n_comp,c,k,rs):
-        steps = [('pca',PCA(n_components=n_comp)),('SVM',SVC(C=c,gamma='scale',kernel=k,random_state=rs))]
+    def _make_pipeline(self,n_comp,n,lr,md,ss):
+        steps = [('pca',PCA(n_components=n_comp)),('GBC',GradientBoostingClassifier(n_estimators=n,learning_rate=lr,max_depth=md,subsample=ss))]
         pipe = Pipeline(steps)
         return pipe

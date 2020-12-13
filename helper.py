fa56fcc02b6bb9d90269575729e1e356a8116d5c
deeprob <deepro.2.4@gmail.com>
2020-12-13 15:17:17 -0500

    AutoEncoderAdded

--- helper.py
+++ helper.py
@@ -38,3 +38,55 @@ def modified_split(enzyme_names,split=0.25):
     
 
     return test_split_idx,train_split_idx 
+
+def balancing_function(X_tr,y_tr):
+    values,counts = np.unique(y_tr,return_counts=True)
+    label_count_dict = {l:c for l,c in zip(values,counts)}
+    
+    # get the max count
+    max_count = max(label_count_dict.values())
+    
+    # for the labels with less max count, randomly select their index and append that index to the array
+    
+    for k,v in label_count_dict.items():
+        if v<max_count:
+            #get the indices with those values
+            indices = np.where(y_tr==k)[0]
+            
+            # get how training set must be duplicated
+            n_duplicate = max_count-v
+            
+            if n_duplicate>v:
+                
+                times_to_add = n_duplicate//v 
+                    
+                X_tr_k = X_tr[indices,:]
+                
+                for i in range(times_to_add):
+                    X_tr = np.concatenate((X_tr,X_tr_k),axis=0)
+                    y_tr = np.append(y_tr,np.array([k for i in range(v)]))
+                
+                remaining_times = n_duplicate-times_to_add*v
+                
+                random_indices = np.random.choice(indices,size=remaining_times)
+                
+                X_tr_extra = X_tr[random_indices,:]
+                y_tr_extra = np.array([k for i in range(remaining_times)])
+                
+                X_tr = np.concatenate((X_tr,X_tr_extra),axis=0)
+                y_tr = np.append(y_tr,y_tr_extra)
+                
+            else:
+                
+                remaining_times = n_duplicate
+                
+                random_indices = np.random.choice(indices,size=remaining_times)
+                
+                X_tr_extra = X_tr[random_indices,:]
+                y_tr_extra = np.array([k for i in range(remaining_times)])
+                
+                X_tr = np.concatenate((X_tr,X_tr_extra),axis=0)
+                y_tr = np.append(y_tr,y_tr_extra)
+                
+                
+    return X_tr,y_tr

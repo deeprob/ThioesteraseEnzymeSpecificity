de8f1f99b27a04777cfbf1fc542253c050e2f8c6
deeprob <deepro.2.4@gmail.com>
2021-01-12 17:48:03 -0500

    simulations-cluster

--- Ensemble/model.py
+++ Ensemble/model.py
@@ -2,17 +2,14 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 
 class Ensemble:
-    def __init__(self,models,Xtests,ytest):
-        self.all_models = models
-        self.Xtests = Xtests
+    def __init__(self,modelpreds,ytest):
         self.ytest = ytest
-        
+        self.modelpreds = modelpreds
         self.preds = np.array(self._get_predictions())
         self.acc = accuracy_score(self.ytest,self.preds)
         pass
     
     def _get_predictions(self):
-        predictions = [m.predict(Xtest) for m,Xtest in zip(self.all_models,self.Xtests)]
-        preds = [np.argmax(np.bincount(np.array(tup))) for tup in zip(*predictions)]
+        preds = [np.argmax(np.bincount(np.array(tup))) for tup in zip(*self.modelpreds)]
         return preds
         

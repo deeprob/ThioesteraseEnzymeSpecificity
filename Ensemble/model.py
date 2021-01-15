afdb27dabb4a28c5e7149164b29a51001fc224a0
deeprob <deepro.2.4@gmail.com>
2021-01-15 14:16:45 -0700

    reg-label-error-fixed

--- Ensemble/model.py
+++ Ensemble/model.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,mean_squared_error
 import numpy as np
 
 class Ensemble:
@@ -12,4 +12,16 @@ class Ensemble:
     def _get_predictions(self):
         preds = [np.argmax(np.bincount(np.array(tup))) for tup in zip(*self.modelpreds)]
         return preds
-        
+
+class EnsembleRegression:
+    def __init__(self,modelpreds,ytest):
+        self.ytest = ytest
+        self.modelpreds = modelpreds
+        self.preds = np.array(self._get_predictions())
+        self.mse = mean_squared_error(self.ytest,self.preds)
+        pass
+    
+    def _get_predictions(self):
+        preds = [np.mean(tup) for tup in zip(*self.modelpreds)]
+        return preds
+ 
